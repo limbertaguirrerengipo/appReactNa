a6950ffda8611b6744ef7e8c3c961db3ba77a193
laguirre <laguirre@gruposion.bo>
2022-02-19 21:57:54 -0400

    login statico

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -8,6 +8,7 @@ import {
     Text,
     useColorScheme,
     View,
+    Image,
 } from 'react-native';
 
 import {
@@ -17,10 +18,12 @@ import {
     LearnMoreLinks,
     ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { Card, Input  } from "react-native-elements";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const LoginScreen = () => {
+    
     const isDarkMode = useColorScheme() === 'dark';
-
     const backgroundStyle = {
         backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     };
@@ -36,9 +39,53 @@ const LoginScreen = () => {
                     style={{
                         backgroundColor: isDarkMode ? Colors.black : Colors.white,
                     }}>
-
-                    <Text style={styles.highlight}>Home base sin login g d..</Text>
-
+                    <Card containerStyle={{}} wrapperStyle={{}}>
+                    <Card.Title style={styles.titulo}>LOGIN</Card.Title>                    
+                    <View style={styles.contenedor}>
+                        <Image
+                        style={styles.image}
+                       
+                        resizeMode="contain"
+                        source={{
+                            uri:
+                            "https://img2.freepng.es/20180319/lde/kisspng-india-login-computer-icons-emoticon-medicine-user-login-icon-5ab05c8be74df7.6661394815215074679474.jpg"
+                        }}
+                        />                        
+                        <Input
+                        containerStyle={{}}
+                        disabledInputStyle={{ background: "#ddd" }}
+                        inputContainerStyle={{}}
+                        errorMessage=""
+                        errorStyle={{}}
+                        errorProps={{}}
+                        inputStyle={{}}
+                        label="Nombre usuario"
+                        labelStyle={{}}
+                        labelProps={{}}
+                        leftIcon={<Icon name="account-outline" size={20} />}
+                        leftIconContainerStyle={{}}
+                        rightIconContainerStyle={{}}
+                        placeholder="Usuario"
+                        />
+                        <Input
+                        containerStyle={{}}
+                        disabledInputStyle={{ background: "#ddd" }}
+                        inputContainerStyle={{}}
+                        errorMessage=""
+                        errorStyle={{}}
+                        errorProps={{}}
+                        inputStyle={{}}
+                        label="Contraseña"
+                        labelStyle={{}}
+                        labelProps={{}}
+                        leftIcon={<Icon name="account-outline" size={20} />}
+                        leftIconContainerStyle={{}}
+                        rightIconContainerStyle={{}}
+                        placeholder="Contraseña"
+                        secureTextEntry={true}
+                        />
+                    </View>
+                    </Card>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -49,6 +96,20 @@ const styles = StyleSheet.create({
     highlight: {
         fontWeight: '700',
     },
+    titulo: {
+        marginTop: 20,
+        marginBottom: 30,
+    },
+    image: {
+        marginTop: 1,
+        marginBottom: 20,
+        width: "100%", 
+        height: 100 
+    },
+    contenedor: {
+        position: "relative",
+        alignItems: "center"
+    }
 });
 
 export default LoginScreen;
